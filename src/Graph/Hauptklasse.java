bd4befda84647416e48b2510edb2a145dacb4574
ala <Anton-Lammert@gmx.de>
2017-04-18 15:30:25 +0200

    Removed unused imports

--- src/Graph/Hauptklasse.java
+++ src/Graph/Hauptklasse.java
@@ -2,10 +2,7 @@ package Graph;
 
 import java.io.IOException;
 
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
